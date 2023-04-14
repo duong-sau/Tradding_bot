6de0ee8b07cc9f5cf4027746716419dfa9d6124f
duong sau <duongsau1211@gmail.com>
2023-04-14 23:34:44 +0700

    Disable button

--- View/a_common/Button/Button.py
+++ View/a_common/Button/Button.py
@@ -5,4 +5,13 @@ class Button(QPushButton):
     def __init__(self, title, color, parent=None):
         super().__init__(parent)
         self.setText(title)
+        self.color = color
         self.setStyleSheet(f"background-color: {color}; font-size: 24px")
+
+    def disable(self):
+        self.setEnabled(False)
+        self.setStyleSheet(f"background-color: #000000; font-size: 24px")
+
+    def enable(self):
+        self.setEnabled(True)
+        self.setStyleSheet(f"background-color: {self.color}; font-size: 24px")
--- View/control_view.py
+++ View/control_view.py
@@ -8,6 +8,7 @@ class ControlView(QWidget):
         super(ControlView, self).__init__(parent)
         self.long_button = Button(" LONG ", '#00FF00')
         self.short_button = Button(" SHORT ", '#FF0000')
+        self.short_button.disable()
         self.create_layout()
 
     def create_layout(self):
