f00066d24e653d5d957cde347c461ef4363378b4
duong sau <duongsau1211@gmail.com>
2023-04-15 16:54:34 +0700

    finall release

--- Binance/Common.py
+++ Binance/Common.py
@@ -3,6 +3,10 @@ import exrex
 
 def get_limit_from_parameter(symbol, quantity, price, margin, side):
     order_id = exrex.getone(r'vduongsauv[a-z0-9]{12}')
+    if side == "BUY":
+        position_side = "LONG"
+    else:
+        position_side = "SHORT"
     order_param = {
         'symbol': symbol,
         'side': side,
@@ -11,7 +15,7 @@ def get_limit_from_parameter(symbol, quantity, price, margin, side):
         'leverage': margin,
         'type': 'LIMIT',
         'newClientOrderId': order_id,
-        'reduceOnly': False,
+        'positionSide': position_side,
         'timeInForce': 'GTC',
         'newOrderRespType': "ACK"
     }
